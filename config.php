0d695634907d9c1d5a744fec6ad29e8c3b9b5110
aryanpandey8047 <aryanpandey9839@gmail.com>
2025-03-06 03:03:00 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$db_name="assessment";
+$conn=new mysqli($servername,$username,$password,$db_name);
+if(!$conn){
+    die("Connection failed:".$conn->connect_error);
+}
+else
+echo "Connected to database!";
+?>
