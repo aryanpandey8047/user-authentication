f62c1e007909a770592e4c76db85a1abfe0500c4
aryanpandey8047 <aryanpandey9839@gmail.com>
2025-03-06 03:40:01 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,16 +2,8 @@
 session_start();
 include "config.php";
 
-$stmt = $conn->prepare("SELECT COUNT(*) FROM client");//TO Check if the database has any records
-$stmt->execute();
-$stmt->bind_result($user_count);
-$stmt->fetch();
-$stmt->close();
-?>
 
-<h2>Register</h2>
-
-<?php if ($user_count > 0)
+    if ($user_count > 0)
     echo "<p style='color: blue;''><a href='login.php'> Log in here</a></p>";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $firstname = trim($_POST['firstname']);
@@ -19,7 +11,21 @@ $stmt->close();
         $username = trim($_POST['username']);
         $password = trim($_POST['password']);
     
-        // Insert new user into the database
         $stmt = $conn->prepare("INSERT INTO client (first_name, last_name, username, password) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssss", $firstname, $lastname, $username, $password);
+
+        if ($stmt->execute()) {
+            echo "<p style='color:green;'>Registration successful! </p>";
+        } else {
+            echo "<p style='color:red;'>Error: Could not register user.</p>";
+        }
+    }
 ?>
+
+<form method="POST">
+    <input type="text" name="firstname" placeholder="First name" required><br>
+    <input type="text" name="lastname" placeholder="Last name" required><br>
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Register</button>
+</form>
